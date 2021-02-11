3e1244709b034ad0ea0a7863f17aa3126725cd20
Sello Fotoyi <selloclinton@gmail.com>
2021-02-11 12:22:28 +0200

    about v1

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,8 +1,38 @@
-    var counter = 1;
-    setInterval(function(){
-      document.getElementById('radio' + counter).checked = true;
-      counter++;
-      if(counter > 5){
-        counter = 1;
+    // var counter = 1;
+    // setInterval(function(){
+    //   document.getElementById('radio' + counter).checked = true;
+    //   counter++;
+    //   if(counter > 5){
+    //     counter = 1;
+    //   }
+    // }, 5000);
+
+    //history img slider
+    let slides = document.querySelector('.about-slides');
+    let prev_btn = document.querySelector('.prev-btn');
+     let nxt_btn = document.querySelector('.next-btn');
+    let h_counter = 0;
+    prev_btn.addEventListener('click',()=>{
+      h_counter--;
+      if(h_counter < 0){
+        h_counter = 0;
       }
-    }, 5000);
+      if(h_counter == 0){
+         prev_btn.style.opacity = '1';
+      }
+
+      slides.style.marginLeft = -h_counter+'00vw';
+      
+    });
+
+    nxt_btn.addEventListener('click',()=>{
+      h_counter++;
+      if(h_counter > 4){
+        h_counter = 0;
+      }
+
+      slides.style.marginLeft = -h_counter+'00vw';
+        
+    });
+    //history img slider-end
+    
