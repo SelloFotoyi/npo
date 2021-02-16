7cd52043c07de50e36eea6ee1839228b16e7fed8
Sello Fotoyi <selloclinton@gmail.com>
2021-02-16 17:18:34 +0200

    about fixed after testing

--- dist/js/script.js
+++ dist/js/script.js
@@ -8,32 +8,50 @@
     // }, 5000);
 
     //history img slider
-    // let slides = document.querySelector('.about-slides');
-    // let prev_btn = document.querySelector('.prev-btn');
-    //  let nxt_btn = document.querySelector('.next-btn');
-    // let h_counter = 0;
-    // prev_btn.addEventListener('click',()=>{
-    //   h_counter--;
-    //   if(h_counter < 0){
-    //     h_counter = 0;
-    //   }
-    //   if(h_counter == 0){
-    //      prev_btn.style.opacity = '1';
-    //   }
-
-    //   slides.style.marginLeft = -h_counter+'00vw';
+    let slides = document.querySelector('.about-slides');
+    let prev_btn = document.querySelector('.prev-btn');
+    let prev_btn_arrow = document.querySelector('.fa-arrow-circle-left')
+    let nxt_btn = document.querySelector('.next-btn');
+    let nxt_btn_arrow = document.querySelector('.fa-arrow-circle-right')
+    let h_counter = 0;
+    prev_btn_arrow.style.opacity = '0.5';
+    prev_btn.addEventListener('click',()=>{
+      h_counter--;
+      if(h_counter < 0){
+        h_counter = 0;
+      }
+      if(h_counter <= 0){
+        prev_btn_arrow.style.transition = '1s';
+        prev_btn_arrow.style.opacity = '0.5';
+      }
+      else{
+        prev_btn_arrow.style.transition = '1s';
+        prev_btn_arrow.style.opacity = '1';
+      }
       
-    // });
 
-    // nxt_btn.addEventListener('click',()=>{
-    //   h_counter++;
-    //   if(h_counter > 4){
-    //     h_counter = 0;
-    //   }
+      slides.style.marginLeft = -h_counter+'00vw';
+      
+    });
 
-    //   slides.style.marginLeft = -h_counter+'00vw';
+    nxt_btn.addEventListener('click',()=>{
+      h_counter++;
+      if(h_counter > 4){
+        h_counter = 0;
+      }
+
+      if(h_counter <= 0){
+        prev_btn_arrow.style.transition = '1s';
+        prev_btn_arrow.style.opacity = '0.5';
+      }
+      else{
+        prev_btn_arrow.style.transition = '1s';
+        prev_btn_arrow.style.opacity = '1';
+      }
+
+      slides.style.marginLeft = -h_counter+'00vw';
         
-    // });
+    });
     //history img slider-end
 
     //*********stories handler***********/
